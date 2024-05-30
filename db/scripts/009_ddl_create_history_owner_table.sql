848a8d177bfdebc078b3461b6a6dbcac73ab8086
origin <git@github.com:OljaKn/job4j_design.git>
2024-05-30 14:50:23 +0300

    6. Модели и связи. Машины и владельцы

--- db/scripts/009_ddl_create_history_owner_table.sql
+++ db/scripts/009_ddl_create_history_owner_table.sql
@@ -1,7 +1,7 @@
 create table history_owner(
     id serial primary key,
     owner_id int not null references owners(id),
-    car_id int not null references car(id),
+    car_id int not null references cars(id),
     startAt timestamp,
     endAt timestamp,
     unique (owner_id, car_id)
